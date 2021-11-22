a441ffe1eccdf69a1654e8530ef7c6212543c4c6
AlphaDx <loco.cansado@gmail.com>
2021-11-22 15:12:04 +0000

    Variables de entorno para la conexión

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=' . getenv("DBHOST") . ';dbname=' . getenv("DBNAME") ,
+    'username' => getenv("DBUSERNAME"),
+    'password' => getenv("DBPASSWORD"),
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
