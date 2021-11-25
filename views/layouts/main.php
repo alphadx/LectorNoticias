13757c2045dd4833795c49e3c6fccfe684956eb5
AlphaDx <loco.cansado@gmail.com>
2021-11-25 04:15:33 +0000

    Fuente AWESOME

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -17,6 +17,7 @@ AppAsset::register($this);
 <html lang="<?= Yii::$app->language ?>" class="h-100">
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
